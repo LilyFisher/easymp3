2f770a23e6dd2deaabc05f4b7d7cdda30981b7a4
Fisher, Alicja <fisherlily4@gmail.com>
2023-02-01 18:21:31 -0600

    spring init

---
+++ easymp3/src/main/java/com/fisher/easymp3/Easymp3Application.java
@@ -0,0 +1,13 @@
+package com.fisher.easymp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Easymp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Easymp3Application.class, args);
+	}
+
+}
---
+++ easymp3/src/test/java/com/fisher/easymp3/Easymp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisher.easymp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Easymp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
